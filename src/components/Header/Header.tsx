41516a7316c941f1b2bc8df79f434d98125d8bac
jkica <jki@valuer.ai>
2023-01-21 12:36:48 +0100

    refactor header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,39 +1,22 @@
 import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 // components
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 
 //assets
-import Logo from '../../assets/icons/logo.svg'
-import './style.css'
-
-const pages = [
-    {
-        name: 'All Posts',
-        url: '/'
-    },
-    {
-        name: 'Add a Post',
-        url: '/add'
-    },
-    {
-        name: 'Edit a Post',
-        url: '/edit/1'
-    }];
+import Logo from '../../assets/icons/logo.svg';
+import './style.css';
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 
 export const Header = () => {
-    const navigate = useNavigate();
-
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
+                <Toolbar className="header-wrapper" disableGutters>
                     <Typography
                         variant="h6"
                         noWrap
@@ -41,19 +24,15 @@ export const Header = () => {
                         href="/">
                         <img src={Logo} alt="logo"/>
                     </Typography>
-                    <Box>
-                        {
-                            pages.map((page) => (
-                                <Button
-                                    className="header-btn"
-                                    key={page.url}
-                                    onClick={() => navigate(`${page.url}`)}>
-                                    {page.name}
-                                </Button>
-                            ))
-                        }
-                    </Box>
-                    
+                    <Button
+                        href="/add"
+                        className="header-create-btn"
+                        color="success"
+                        size="large"
+                        endIcon={<AddCircleOutlineRoundedIcon />}
+                        variant="contained">
+                        Create Post
+                    </Button>
                 </Toolbar>
             </Container>
         </AppBar>
